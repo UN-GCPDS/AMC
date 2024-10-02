98b067c5ac6d9dcf527180bc41680afa288cebf5
jclugor <jclugor@unal.edu.co>
2024-10-02 17:22:54 -0500

    update

--- mltools.py
+++ mltools.py
@@ -12,19 +12,19 @@ def show_history(history):
     plt.plot(history.epoch, history.history['loss'], label='train loss+error')
     plt.plot(history.epoch, history.history['val_loss'], label='val_error')
     plt.legend()
-    plt.savefig('figure/total_loss.png')
+    plt.savefig('figure/total_loss.pdf')
     plt.close()
  
 
     plt.figure()
     plt.title('Training accuracy performance')
-    plt.plot(history.epoch, history.history['acc'], label='train_acc')
-    plt.plot(history.epoch, history.history['val_acc'], label='val_acc')
+    plt.plot(history.epoch, history.history['accuracy'], label='train_acc')
+    plt.plot(history.epoch, history.history['val_accuracy'], label='val_acc')
     plt.legend()    
-    plt.savefig('figure/total_acc.png')
+    plt.savefig('figure/total_acc.pdf')
     plt.close()
 
-    train_acc=history.history['acc']
+    train_acc=history.history['accuracy']
     val_acc=history.history['val_acc']
     train_loss=history.history['loss']
     val_loss=history.history['val_loss']
@@ -34,38 +34,58 @@ def show_history(history):
     np_train_loss=np.array(train_loss)
     np_val_loss=np.array(val_loss)
     np_epoch=np.array(epoch)
-    np.savetxt('train_acc.txt',np_train_acc,delimiter=',')
-    np.savetxt('train_loss.txt',np_train_loss,delimiter=',')
-    np.savetxt('val_acc.txt',np_val_acc,delimiter=',')
-    np.savetxt('val_loss.txt',np_val_loss,delimiter=',')
+    np.savetxt('train_acc.txt',np_train_acc)
+    np.savetxt('train_loss.txt',np_train_loss)
+    np.savetxt('val_acc.txt',np_val_acc)
+    np.savetxt('val_loss.txt',np_val_loss)
 
+def plot_lstm2layer_output(a,modulation_type=None,save_filename=None):
+    plt.figure(figsize=(4,3),dpi=600)
+    plt.plot(range(128),a[0],label=modulation_type)
+    plt.legend()
+    plt.xticks([])  #去掉横坐标值
+    plt.yticks([])
+    plt.savefig(save_filename,dpi=600,bbox_inches ='tight')
+    plt.tight_layout()
+    plt.close()
+
+def plot_conv4layer_output(a,modulation_type=None):
+    plt.figure(figsize=(4,3),dpi=600)
+    for i in range(100):
+        plt.plot(range(124),a[0,0,:,i])
+        plt.xticks([])  #去掉横坐标值
+        plt.yticks(size=20)
+        save_filename='./figure_conv4_output/output%d.pdf'%i
+        plt.savefig(save_filename,dpi=600,bbox_inches='tight')
+        plt.tight_layout()
+        plt.close()
+ 
 
 def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.get_cmap("Blues"), labels=[],save_filename=None):
-    plt.figure(figsize=(4, 3), dpi=600)
-    plt.imshow(cm * 100, interpolation='nearest', cmap=cmap)
-    # plt.title(title,fontsize=10)
+    plt.figure(figsize=(4, 3),dpi=600)
+    plt.imshow(cm*100, interpolation='nearest', cmap=cmap)
+    #plt.title(title,fontsize=10)
     plt.colorbar()
     tick_marks = np.arange(len(labels))
-    plt.xticks(tick_marks, labels, rotation=90, size=12)
-    plt.yticks(tick_marks, labels, size=12)
-    # np.set_printoptions(precision=2, suppress=True)
+    plt.xticks(tick_marks, labels, rotation=90,size=12)
+    plt.yticks(tick_marks, labels,size=12)
+    #np.set_printoptions(precision=2, suppress=True)
     for i in range(len(tick_marks)):
         for j in range(len(tick_marks)):
-            if i != j:
-                text = plt.text(j, i, int(np.around(cm[i, j] * 100)), ha="center", va="center", fontsize=10)
-            elif i == j:
-                if int(np.around(cm[i, j] * 100)) == 100:
-                    text = plt.text(j, i, int(np.around(cm[i, j] * 100)), ha="center", va="center", fontsize=7,
-                                    color='darkorange')
+            if i!=j:
+                text=plt.text(j,i,int(np.around(cm[i,j]*100)),ha="center",va="center",fontsize=10)
+            elif i==j:
+                if int(np.around(cm[i,j]*100))==100:
+                    text=plt.text(j,i,int(np.around(cm[i,j]*100)),ha="center",va="center",fontsize=7,color='darkorange')
                 else:
-                    text = plt.text(j, i, int(np.around(cm[i, j] * 100)), ha="center", va="center", fontsize=10,
-                                    color='darkorange')
+                    text=plt.text(j,i,int(np.around(cm[i,j]*100)),ha="center",va="center",fontsize=10,color='darkorange')
+            
 
     plt.tight_layout()
-    # plt.ylabel('True label',fontdict={'size':8,})
-    # plt.xlabel('Predicted label',fontdict={'size':8,})
+    #plt.ylabel('True label',fontdict={'size':8,})
+    #plt.xlabel('Predicted label',fontdict={'size':8,})
     if save_filename is not None:
-        plt.savefig(save_filename, dpi=600, bbox_inches='tight')
+        plt.savefig(save_filename,dpi=600,bbox_inches = 'tight')
     plt.close()
 
 def calculate_confusion_matrix(Y,Y_hat,classes):
