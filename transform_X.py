fc9b6297a77c6e3d60e7557846a1e1197245b0aa
jclugor <jclugor@unal.edu.co>
2024-11-22 09:06:12 -0500

    include x_train in transform

--- transform_X.py
+++ transform_X.py
@@ -84,80 +84,106 @@ def transform(X, Y, model_):
     if model_ in ['GRU2']:
         X_train_ = X_train_.swapaxes(2, 1)
         X_val_ = X_val_.swapaxes(2, 1)
+        X_test_ = X_test_.swapaxes(2, 1)
     elif model_ in ['LSTM2']:
         X_train_, X_val_, X_test_ = to_amp_phase(X_train_, X_val_, X_test_, 128)
         X_train_ = norm_pad_zeros(X_train_[:, :128, :], 128)
         X_val_ = norm_pad_zeros(X_val_[:, :128, :], 128)
+        X_test_ = norm_pad_zeros(X_test_[:, :128, :], 128)
     elif model_ in ['1DCNN-PF']:
         X_train_, X_val_, X_test_ = to_amp_phase(X_train_, X_val_, X_test_, 128)
 
         X_train_ = norm_pad_zeros(X_train_[:, :128, :], 128)
         X_val_ = norm_pad_zeros(X_val_[:, :128, :], 128)
+        X_test_ = norm_pad_zeros(X_test_[:, :128, :], 128)
         
         X1_train = X_train_[:, :, 0]
         X1_val = X_val_[:, :, 0]
+        X1_test = X_test_[:, :, 0]
         X2_train = X_train_[:, :, 1]
         X2_val = X_val_[:, :, 1]
+        X2_test = X_test_[:, :, 1]
         X_train_ = [X1_train, X2_train]
         X_val_ = [X1_val, X2_val]
+        X_test_ = [X1_test, X2_test]
         
     elif model_ in ['CGDNet']:
         X_train_ = np.expand_dims(X_train_, axis=1)
         X_val_ = np.expand_dims(X_val_, axis=1)
+        X_test_ = np.expand_dims(X_test_, axis=1)
         
     elif model_ in ['CLDNN']:
         X_train_ = np.reshape(X_train_, (-1, 1, 2, 128))
         X_val_ = np.reshape(X_val_, (-1, 1, 2, 128))
+        X_test_ = np.reshape(X_test_, (-1, 1, 2, 128))
         
     elif model_ in ['CLDNN2', 'DenseNet', 'IC-AMCNet', 'ResNet', 'TAD']:
         X_train_ = np.expand_dims(X_train_, axis=3)
         X_val_ = np.expand_dims(X_val_, axis=3)
+        X_test_ = np.expand_dims(X_test_, axis=3)
                                
     elif model_ in ['DAE']:
         X_train_, X_val_, X_test_ = to_amp_phase(X_train_, X_val_, X_test_, 128)
         X_train_[:, :, 0] = l2_normalize(X_train_[:, :, 0])
         X_val_[:, :, 0] = l2_normalize(X_val_[:, :, 0])
+        X_test_[:, :, 0] = l2_normalize(X_test_[:, :, 0])
         for i in range(X_train_.shape[0]):
             k = 2/(X_train_[i,:,1].max() - X_train_[i,:,1].min())
         X_train_[i,:,1]=-1+k*(X_train_[i,:,1]-X_train_[i,:,1].min())
         for i in range(X_val_.shape[0]):
             k = 2/(X_val_[i,:,1].max() - X_val_[i,:,1].min())
         X_val_[i,:,1]=-1+k*(X_val_[i,:,1]-X_val_[i,:,1].min())
+        for i in range(X_test_.shape[0]):
+            k = 2/(X_test_[i,:,1].max() - X_test_[i,:,1].min())
+        X_test_[i,:,1]=-1+k*(X_test_[i,:,1]-X_test_[i,:,1].min())
 
     elif model_ in ['MCLDNN']:
         X1_train = np.expand_dims(X_train_[:, 0, :], axis=2)
         X1_val = np.expand_dims(X_val_[:, 0, :], axis=2)
+        X1_test = np.expand_dims(X_test_[:, 0, :], axis=2)
         X2_train = np.expand_dims(X_train_[:, 1, :], axis=2)
         X2_val = np.expand_dims(X_val_[:, 1, :], axis=2)
+        X2_test = np.expand_dims(X_test_[:, 1, :], axis=2)
         X_train_t = np.expand_dims(X_train_, axis=3)
         X_val_t = np.expand_dims(X_val_, axis=3)        
+        X_test_t = np.expand_dims(X_test_, axis=3)        
         X_train_ = [X_train_t, X1_train, X2_train]
         X_val_ = [X_val_t, X1_val, X2_val]
+        X_test_ = [X_test_t, X1_test, X2_test]
 
     elif model_ in ['MCNET']:
         X_train_ = np.expand_dims(X_train_, axis=3)
         X_val_ = np.expand_dims(X_val_, axis=3)
+        X_test_ = np.expand_dims(X_test_, axis=3)
 
     elif model_ in ['PET-CGDNN']:
         X_train_ = X_train_.swapaxes(2, 1)
         X_val_ = X_val_.swapaxes(2, 1)
+        X_test_ = X_test_.swapaxes(2, 1)
         X1_train = X_train_[:, :, 0]
         X2_train = X_train_[:, :, 1]
         X1_val = X_val_[:, :, 0]
         X2_val = X_val_[:, :, 1]
+        X1_test = X_test_[:, :, 0]
+        X2_test = X_test_[:, :, 1]
         X_train_ = np.expand_dims(X_train_, axis=3)
         X_val_ = np.expand_dims(X_val_, axis=3)
+        X_test_ = np.expand_dims(X_test_, axis=3)
         X_train_ = [X_train_, X1_train, X2_train]
         X_val_ = [X_val_, X1_val, X2_val]
+        X_test_ = [X_test_, X1_test, X2_test]
     else:
         X_train_ = X_train
         X_val_ = X_val
+        X_test_ = X_test
 
     if model_ in ['DAE']:
         Y_train_ = [Y_train, X_train_]
         Y_val_ = [Y_val, X_val_]
+        Y_test_ = [Y_test, X_test_]
     else:
         Y_train_ = Y_train
         Y_val_ = Y_val
+        Y_test_ = Y_test
 
-    return [X_train, X_val_, X_test_], [Y_train_, Y_val_, Y_test]
+    return [X_train, X_val_, X_test_], [Y_train_, Y_val_, Y_test_]
